d5b1edd94a73537851a9371c45ab3e45608baaea
Shahbaz Ahmed <ashabaz845@gmail.com>
2024-08-18 12:49:56 +0500

    Completed Task

---
+++ app.js
@@ -0,0 +1,17 @@
+function Increase(){
+    let counter = document.getElementById("counter")
+
+    counter.innerHTML++;
+}
+
+function Decrease(){
+    let counter = document.getElementById("counter")
+
+    counter.innerHTML--;
+}
+
+function Reset(){
+    let counter = document.getElementById("counter")
+
+    counter.innerHTML = 0;
+}
